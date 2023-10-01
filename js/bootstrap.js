b768912d4f0652da21fe904f89e6e748d93a5a6b
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2023-10-01 03:33:27 -0400

    Remove keframes mixin and refactor js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,29 +1,20 @@
 /* global document */
 
-// export { default as Alert } from 'bootstrap/js/src/alert.js';
-// export { default as Button } from 'bootstrap/js/src/button.js';
-// export { default as Carousel } from 'bootstrap/js/src/carousel.js';
-// export { default as Collapse } from 'bootstrap/js/src/collapse.js';
-// export { default as Dropdown } from 'bootstrap/js/src/dropdown.js';
+export { default as Alert } from 'bootstrap/js/src/alert.js';
+export { default as Button } from 'bootstrap/js/src/button.js';
+export { default as Carousel } from 'bootstrap/js/src/carousel.js';
+export { default as Collapse } from 'bootstrap/js/src/collapse.js';
+export { default as Dropdown } from 'bootstrap/js/src/dropdown.js';
 // export { default as Modal } from './modal.js';
-// export { default as Offcanvas } from 'bootstrap/js/src/offcanvas.js';
+export { default as Offcanvas } from 'bootstrap/js/src/offcanvas.js';
 // export { default as Popover } from 'bootstrap/js/src/popover.js';
-// export { default as ScrollSpy } from 'bootstrap/js/src/scrollspy.js';
-// export { default as Tab } from 'bootstrap/js/src/tab.js';
-// export { default as Toast } from 'bootstrap/js/src/toast.js';
+export { default as ScrollSpy } from 'bootstrap/js/src/scrollspy.js';
+export { default as Tab } from 'bootstrap/js/src/tab.js';
+export { default as Toast } from 'bootstrap/js/src/toast.js';
 // export { default as Tooltip } from 'bootstrap/js/src/tooltip.js';
 
-import Alert from 'bootstrap/js/src/alert.js';
-import Button from 'bootstrap/js/src/button.js';
-import Carousel from 'bootstrap/js/src/carousel.js';
-import Collapse from 'bootstrap/js/src/collapse.js';
-import Dropdown from 'bootstrap/js/src/dropdown.js';
 import setupModal from './modal.js';
-import Offcanvas from 'bootstrap/js/src/offcanvas.js';
 import setupPopover from './popover.js';
-import ScrollSpy from 'bootstrap/js/src/scrollspy.js';
-import Tab from 'bootstrap/js/src/tab.js';
-import Toast from 'bootstrap/js/src/toast.js';
 import setupTooltip from './tooltip.js';
 
 export default function (rootElement) {
@@ -34,17 +25,8 @@ export default function (rootElement) {
 	}
 
 	return {
-		Alert,
-		Button,
-		Carousel,
-		Collapse,
-		Dropdown,
 		Modal: setupModal(rootElement),
-		Offcanvas,
 		Popover: setupPopover(rootElement),
-		ScrollSpy,
-		Tab,
-		Toast,
 		Tooltip: setupTooltip(rootElement)
 	};
 }
--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 /* global document */
 
 import './test.scss';
-import setupBootstrap from '../js/bootstrap.js';
+import setupRootElement, { Toast } from '../js/bootstrap.js';
 
-const Bootstrap = setupBootstrap('wrapper');
+const { Popover, Tooltip } = setupRootElement('wrapper');
 
 // Popovers
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new Bootstrap.Popover(popoverTriggerEl)); // eslint-disable-line no-unused-vars
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new Popover(popoverTriggerEl)); // eslint-disable-line no-unused-vars
 
 // Toasts
 
@@ -16,7 +16,7 @@ const toastTrigger = document.getElementById('liveToastBtn');
 const toastLiveExample = document.getElementById('liveToast');
 
 if (toastTrigger) {
-	const toastBootstrap = Bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+	const toastBootstrap = Toast.getOrCreateInstance(toastLiveExample);
 	toastTrigger.addEventListener('click', () => {
 		toastBootstrap.show();
 	});
@@ -25,4 +25,4 @@ if (toastTrigger) {
 // Tooltips
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Bootstrap.Tooltip(tooltipTriggerEl)); // eslint-disable-line no-unused-vars
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl)); // eslint-disable-line no-unused-vars
